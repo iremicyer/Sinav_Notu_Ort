853482a3d2965d366bbe6e0010e3bf962cf67e7b
İREM İÇYER <62353089+iremicyer@users.noreply.github.com>
2020-09-02 13:55:59 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace SinavNotuOrtHesapla
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVeriler = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnVeriler
+            // 
+            this.btnVeriler.BackColor = System.Drawing.Color.MediumBlue;
+            this.btnVeriler.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnVeriler.ForeColor = System.Drawing.Color.Transparent;
+            this.btnVeriler.Location = new System.Drawing.Point(186, 54);
+            this.btnVeriler.Name = "btnVeriler";
+            this.btnVeriler.Size = new System.Drawing.Size(156, 63);
+            this.btnVeriler.TabIndex = 0;
+            this.btnVeriler.Text = "Verileri Getir ve Hesapla";
+            this.btnVeriler.UseVisualStyleBackColor = false;
+            this.btnVeriler.Click += new System.EventHandler(this.btnVeriler_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(72, 144);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(168, 251);
+            this.listBox1.TabIndex = 1;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(284, 144);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(181, 251);
+            this.listBox2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.ClientSize = new System.Drawing.Size(539, 450);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnVeriler);
+            this.Name = "Form1";
+            this.Text = "Not Ortalaması";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVeriler;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SinavNotuOrtHesapla
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVeriler_Click(object sender, EventArgs e)
+        {
+            string notlar = "";
+            StreamReader sr = new StreamReader("sinavlar.txt");
+            while (true)
+            {
+                notlar = sr.ReadLine();
+                if( notlar == null)
+                {
+                    break;
+                }
+                listBox1.Items.Add(notlar);
+                string[] sinavNotlari = notlar.Split(' '); // notları ayrı ayrı hesaplmaka için "split" metodu ile string veriyi parçaladık.
+
+                double ort = 0.4 * Convert.ToDouble(sinavNotlari[1]) + 0.6 * Convert.ToDouble(sinavNotlari[2]);
+                listBox2.Items.Add(sinavNotlari[0] + ':' + ort); // >> "listBox2.Items.Add( ort);" yazarsak nota sahip olan kişinin ismi yazmaz.
+
+            }
+
+        }
+    }
+}
